10f477281f5359d7081f37267dd84a0b7e583343
khalh <amtesdam@gmail.com>
2023-04-20 15:57:38 +0700

    add func for dockerservices

--- docker/utils/dockerservices.py
+++ docker/utils/dockerservices.py
@@ -1,17 +1,53 @@
-import docker
-#import os
+import logging
+from docker import DockerClient
+from docker.errors import APIError, NotFound
 
 class DockerInterface:
     def __init__(self):
-        self.cli = docker.DockerClient(base_url='tcp://10.10.24.97:2377')
+        self.cli = DockerClient(base_url='unix://var/run/docker.sock')
+
+        self.auth_config = {
+            'username': 'lekha2809',
+            'password': 'emdixaqua1'
+        }
+
 
     def get_docker_info(self):
         result = self.cli.info()
         return result
 
-    # def get_node_number(self):
-    #     result = self.cli.info()
+    def get_node_number(self):
+        result = self.cli.info()
 
-    #     system_status = result['SystemStatus']
-    #     for row in system_status:
-    #         if row[0] == 
+        system_status = result['Swarm']
+        for row in system_status:
+            if row == 'Nodes':
+                number = int(system_status[row])
+                return number
+        return None
+    
+    def get_list_container(self, filters):
+        return self.cli.containers.list(filters=filters)
+    
+    def pull_image(self, repository, tag):
+        result = self.cli.images.pull(repository=repository, tag = tag, auth_config = self.auth_config)
+        return result
+
+    def delete_image(self, image, force= False):
+        return self.cli.images.remove(image = image, force = True)
+    
+    def get_private_port_container_for_service(self, service_name):
+
+        try:
+            result = self.cli.containers.get(container_id = service_name)
+        except (NotFound, APIError) as ex:
+            logging.warning(ex)
+
+            
+
+
+    def close_connection(self):
+        try:
+            self.cli.close()
+        except APIError as ex:
+            logging.warning(ex)
--- docker/utils/test.py
+++ docker/utils/test.py
@@ -1,4 +1,20 @@
 import docker
-
+import re
 client = docker.DockerClient(base_url='unix://var/run/docker.sock')
-info = client.info()
+
+filters = {
+    #'id': '15e13cc9252b',
+    'name': 'hihi-rundeck'
+
+}
+
+# auth_config = {
+#             'username': 'lekha2809',
+#             'password': 'emdixaqua1'
+#        }
+containers = client.containers.list(filters = filters)
+for container in containers:
+    dict = ((container.attrs['NetworkSettings']))
+    private_port = list(dict['Ports'].keys())[0]
+    print(private_port.split("/")[0])
+client.close()
