4d16298cee682736ce0c1ff86b5c668c1921d56c
Kha Le Hoang <khalh@galaxy.com.vn>
2023-04-24 14:19:11 +0700

    add lum la

--- docker/urls.py
+++ docker/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 
 urlpatterns = [
     path('', views.docker, name='docker'),
     path('history/', views.history, name='history'), 
-    
+    path("accounts/", include("django.contrib.auth.urls")),
+    re_path("^api/deploy/$", views.deploy, name='deploy')
 ]
--- docker/views.py
+++ docker/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 # Create your views here.
-
+@login_required
 def docker(request):
-    return render(request, 'docker/docker.html')
+    return render(request, 'docker/basic.html')
 
 def history(request):
     return render(request, 'docker/history.html')
 
 def login(request):
     return render(request, 'docker/login_page.html')
+
+def deploy(request):
+    return HttpResponse(content='success', status=200)
+
